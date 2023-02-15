974cb692721d93fa299d13ad13f2c4c71990dd97
CanaanW <cbweise4.cw@gmail.com>
2023-02-15 17:25:09 +0000

    adding files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/canaan/catkin_ws/src
+# Build directory: /home/canaan/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3_simulations/turtlebot3_simulations")
+subdirs("beginner_tutorials")
+subdirs("move_turtlebot")
+subdirs("turtlebot3_simulations/turtlebot3_fake")
+subdirs("turtlebot3_simulations/turtlebot3_gazebo")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PATH='/opt/ros/melodic/bin:/home/canaan/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/Program Files (x86)/VMware/VMware Player/bin/:/mnt/c/Program Files/AdoptOpenJDK/jdk-15.0.2.7-hotspot/bin:/mnt/c/Program Files (x86)/Common Files/Oracle/Java/javapath:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/:/mnt/c/WINDOWS/System32/OpenSSH/:/mnt/c/Program Files/Git/cmd:/mnt/c/Program Files (x86)/Pulse Secure/VC142.CRT/X64/:/mnt/c/Program Files (x86)/Pulse Secure/VC142.CRT/X86/:/mnt/c/Users/cbwei/miniforge3:/mnt/c/Users/cbwei/miniforge3/Library/mingw-w64/bin:/mnt/c/Users/cbwei/miniforge3/Library/usr/bin:/mnt/c/Users/cbwei/miniforge3/Library/bin:/mnt/c/Users/cbwei/miniforge3/Scripts:/mnt/c/Users/cbwei/scoop/shims:/mnt/c/Users/cbwei/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/cbwei/.dotnet/tools:/mnt/c/Users/cbwei/AppData/Local/Programs/Microsoft VS Code/bin:/mnt/c/Users/cbwei/AppData/Local/Microsoft/WindowsApps:/mnt/c/Program Files/JetBrains/PyCharm Community Edition 2021.3.2/bin:/snap/bin'
+export PWD='/home/canaan/catkin_ws/build'
